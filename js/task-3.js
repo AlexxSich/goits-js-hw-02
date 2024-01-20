05a4bdf33eb9012f9699c57d72cb3cd2c1f07588
AlexxSich <147013014+AlexxSich@users.noreply.github.com>
2024-01-20 14:23:56 -0400

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,9 @@
+console.log("Task #3 - Element Width")
+
+function getElementWidth(content, padding, border) {
+    return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
